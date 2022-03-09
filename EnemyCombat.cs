9ae795358d5ec2b41c322060d7f2c0e2ced2b2f7
CH <connor-hughes@outlook.com>
2022-03-09 18:52:03 +0000

    Enemies now go to vehicles and do THROW animation

--- EnemyCombat.cs
+++ EnemyCombat.cs
@@ -7,7 +7,7 @@ public class EnemyCombat : MonoBehaviour
 {
     public Transform attackPoint;
     //public Transform detectionPoint;
-    //public Animator animator;
+    public Animator animator;
 
     //public Vector3 detectionRange = new Vector3(6, 1, 6);
     public float attackRange = 0.7f;
@@ -15,12 +15,15 @@ public class EnemyCombat : MonoBehaviour
 
     public float attackRate = 1.5f;
     float nextAttackTime = 0f;
+
     bool m_PlayerInRange = false;
+    bool m_VehicleInRange = false;
+
+    public static Transform detectedPlayer = null;
+    public static Transform detectedVehicle = null;
 
     //GameObject parentObject;
     NavMeshAgent agent;
-    public static Transform detectedPlayer = null;
-
     
     void Start()
     {
@@ -38,7 +41,14 @@ public class EnemyCombat : MonoBehaviour
         {
             m_PlayerInRange = true;
             detectedPlayer = other.transform;
-            Debug.Log("PLAYER ENTER");
+            //Debug.Log("PLAYER ENTER");
+        }
+        if (other.GetComponent<Collider>().CompareTag("Vehicle"))
+        {
+            //Check if Vehicle is destroyed...
+            m_VehicleInRange = true;
+            detectedVehicle = other.transform;
+            //Debug.Log("VEHICLE ENTER");
         }
         //Debug.Log("ENTER "+ other.tag);
     }
@@ -50,6 +60,11 @@ public class EnemyCombat : MonoBehaviour
             m_PlayerInRange = true;
             detectedPlayer = other.transform;
         }
+        if (other.GetComponent<Collider>().CompareTag("Vehicle"))
+        {
+            m_VehicleInRange = true;
+            detectedVehicle = other.transform;
+        }
     }
 
     void OnTriggerExit(Collider other)
@@ -59,6 +74,11 @@ public class EnemyCombat : MonoBehaviour
             m_PlayerInRange = false;
             //Go back to targeting player vehicles
             //Debug.Log("PLAYER EXIT");
+        }
+        if (other.GetComponent<Collider>().CompareTag("Vehicle"))
+        {
+            m_VehicleInRange = false;
+            //Debug.Log("VEHICLE ENTER");
         }
     }
 
@@ -99,6 +119,33 @@ public class EnemyCombat : MonoBehaviour
                 //Debug.Log("NO PLAYERS - RETURN TO TARGETING PLAYER VEHICLE");
                 m_PlayerInRange = false;
             }
+        } 
+        //If Vehicle is in Range
+        else if (m_VehicleInRange)
+        {
+            //Calculate Distance to Player
+            Vector3 vehicleOffset = transform.position - detectedVehicle.position;
+            float vehicleSqrLen = vehicleOffset.sqrMagnitude;
+
+            if (vehicleSqrLen < 4 * 4)
+            {
+                //Close - Attack Vehicle
+                AttackVehicle();
+                
+            }
+            else
+            {
+                //GO BACK TO TARGETING PLAYER VEHICLE
+                //Debug.Log("NO VEHICLES - TARGET NEXT VEHICLE");
+                m_VehicleInRange = false;
+
+            }
+        } 
+        else
+        {
+            //If No vehicle or player in range
+            //Find Closest Vehicle
+            //Find Closest Player
         }
     }
 
@@ -106,7 +153,7 @@ public class EnemyCombat : MonoBehaviour
     {
         //Debug.Log("ATTACK PLAYER()");
         //Play Attack Animation
-        //animator.SetTrigger("Attack");
+        animator.SetTrigger("Attack");
 
         //Detect which enemies were hit
         Collider[] hitPlayers = Physics.OverlapSphere(attackPoint.position, attackRange);
@@ -121,6 +168,28 @@ public class EnemyCombat : MonoBehaviour
                 player.GetComponent<Child>().TakeDamage(attackDamage);
             }
         }
+    }
+
+    void AttackVehicle()
+    {
+        //Debug.Log("ATTACK PLAYER()");
+        //Play Attack Animation
+        animator.SetTrigger("Throw");
+        Debug.Log("ATTACK VEHICLES - THROW");
+        /*
+        //Detect which enemies were hit
+        Collider[] hitVehicles = Physics.OverlapSphere(attackPoint.position, attackRange);
+        //Debug.Log("hitPlayers " + hitPlayers.Length);
+        foreach (var player in hitPlayers)
+        {
+            //Deduct health
+            //hitCollider.SendMessage("AddDamage");
+            if (player.GetComponent<Collider>().CompareTag("Vehicle"))
+            {
+                //Debug.Log("hitPlayer " + player);
+                //vehicle.GetComponent<Child>().TakeDamage(attackDamage);
+            }
+        }*/
     }
 
     void OnDrawGizmosSelected()
